789e23afb455b66728066100207b4b01c9f85b60
KIM BEOMJOON <p1n9@KIMs-MacBook-Air.local>
2024-02-19 16:51:48 +0900

    feat(PrivateRoute): add check login status if user is not logged in then redirect '/login' page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,28 @@
 import Layout from "components/layout/Layout";
+import { useUser } from "contexts/UserContext";
 import Chats from "pages/Chats";
 import Home from "pages/Home";
 import Login from "pages/Login";
 import Users from "pages/Users";
+import { PropsWithChildren, useLayoutEffect } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const PrivateApp = ({ children }: PropsWithChildren) => {
+    const { login } = useUser();
+    useLayoutEffect(() => {
+        if (!login) document.location.href = "/login";
+    }, [login]);
+    return <>{children} </>;
+};
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <Layout />,
+        element: (
+            <PrivateApp>
+                <Layout />
+            </PrivateApp>
+        ),
         children: [
             {
                 index: true,
